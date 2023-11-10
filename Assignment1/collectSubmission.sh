7f341576100b804fcedf17e6f298a404f239d456
oldboy818 <92916644+oldboy818@users.noreply.github.com>
2023-11-11 02:18:58 +0900

    Delete Assignment1 directory

--- Assignment1/collectSubmission.sh
+++
@@ -1,2 +0,0 @@
-rm -f assignment1.zip
-zip -r assignment1.zip . -x "*.git*" "*deeplearning/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*" "*.pyc" "*deeplearning/build/*"
