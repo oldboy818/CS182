72bf250ae32654d6982be9928d7b9102c89ac44d
oldboy818 <92916644+oldboy818@users.noreply.github.com>
2023-12-17 02:30:21 +0900

    Add files via upload

--- assignment3/language_model.py
+++ assignment3/language_model.py
@@ -11,22 +11,25 @@ class LanguageModel(nn.Module):
         # Create an embedding layer of shape [vocab_size, rnn_size]
         # Use nn.Embedding
         # That will map each word in our vocab into a vector of rnn_size size.
-        self.embedding = your_code
+        self.embedding = nn.Embedding(vocab_size, rnn_size)
+                        # shape (vocab_size, rnn_size)의 임베딩 행렬 생성
 
         # Create an LSTM layer of rnn_size size. Use any features you wish.
         # We will be using batch_first convention
-        self.lstm = your_code
+        self.lstm = nn.LSTM(input_size = rnn_size, hidden_size = rnn_size,
+                            num_layers = num_layers)
         # LSTM layer does not add dropout to the last hidden output.
         # Add this if you wish.
 #         self.dropout = your_code
         # Use a dense layer to project the outputs of the RNN cell into logits of
         # the size of vocabulary (vocab_size).
-        self.output =       
+        self.output = nn.Linear(rnn_size, vocab_size)   # in_feature 입력차원이 rnn_size
+                                                        # out_feature 출력차원이 vocab_size
         
     def forward(self,x):
-        embeds = your_code
-        lstm_out, _ = your_code
+        embeds = self.embedding(x)
+        lstm_out, _ = self.lstm(embeds)
 #         lstm_drop = your_code
 #         logits = your_code
-        logits = your_code
+        logits = self.output(lstm_out)
         return logits
