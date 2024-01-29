a395ef1e14cd9c5f81dcecffd297908c62fce5df
oldboy818 <92916644+oldboy818@users.noreply.github.com>
2024-01-29 23:23:24 +0900

    Delete assignment4/hw4_student-master directory

--- assignment4/hw4_student-master/collectSubmission.sh
+++
@@ -1,4 +0,0 @@
-# If this script doesn't work, just manually zip together your Jupter notebooks,
-rm assignment4.zip
-# the deeprl folder, and the logs folder. 
-zip -r assignment4.zip *.ipynb deeprl logs
